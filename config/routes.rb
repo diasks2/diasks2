9c6cca1f148e571e1036541c88e5d74b83c6813c
Kevin <diasks2@gmail.com>
2013-01-01 08:28:41 +0900

    Fix route page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 Diasks2::Application.routes.draw do
   root to: 'static_pages#home'
-  match '/japanese', to: 'static_pages#japanese'
+  match '/rirekisho', to: 'static_pages#rirekisho'
 end
