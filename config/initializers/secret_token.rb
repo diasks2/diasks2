dc9c2afaab2d7e2357ec21296f0c8747623d4454
Kevin <diasks2@gmail.com>
2013-01-01 01:48:13 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diasks2::Application.config.secret_token = 'f7a6ef0738d10b98c1ef76e48c90aa706626476e2177978e0d6eda66b07dce848216edb870e6ea50ac6e4270d49329e6cfb577a097d682582c755cb1b8bab8eb'
