363a6097bbd9ddfefd496ab3d4ab618c25d128e3
Kevin <diasks2@gmail.com>
2013-01-02 02:07:59 +0900

    Work on English version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 	def logo
     	image_tag("diasks2_logo.png", :size => "50x50", :alt => "Kevin Dias")
   	end
+  	def age(dob)
+  		now = Time.now.utc.to_date
+  		now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+	end
 end
